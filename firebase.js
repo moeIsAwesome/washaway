7c4a6a5de0eee6e09f85eb016c9957f320ad7239
Moe Mousavi <99014654+moeIsAwesome@users.noreply.github.com>
2023-03-19 22:24:42 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdY2wD3xzeIvy2OaDEcA03j4KOCv38h0g',
-  authDomain: 'laundry-application-dbd53.firebaseapp.com',
-  projectId: 'laundry-application-dbd53',
-  storageBucket: 'laundry-application-dbd53.appspot.com',
-  messagingSenderId: '453916649073',
-  appId: '1:453916649073:web:d04acb09b19106562bddd0',
+xxxxxxxxxx
 };
 
 // Initialize Firebase
